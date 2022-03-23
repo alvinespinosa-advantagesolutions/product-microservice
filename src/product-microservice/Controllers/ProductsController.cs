e89b1bc30509a621409e67dfa17294407fde5eb9
Alvin Espinosa <AlvinE@magenic.com>
2022-03-23 22:35:25 +0800

    projectv created

---
+++ src/product-microservice/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace product_microservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet(Name = "GetProducts")]
+        public IActionResult Get()
+        {
+            return Ok("products..");
+        }
+    }
+}
